719d8e19bf6f60ccd978a8d5082c8e826b3c0253
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-10-29 17:58:09 -0600

    partially function remove button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskCreateForm = document.getElementById('create-task-form')
+const newTaskInput = document.getElementById('new-task-description')
+const taskList = document.getElementById('tasks')
+
+
+const createNewTask = (event) => {
+  event.preventDefault()
+  
+  const taskFormData = new FormData(event.target);
+  const taskDescription = taskFormData.get('new-task-description')
+  
+  const newTask = document.createElement('li')
+  newTask.innerText = taskDescription
+  
+  const removeButton = document.createElement('button')
+  removeButton.innerText = "X"
+
+  newTask.appendChild(removeButton)
+  taskList.appendChild(newTask)
+  event.target.reset()
+}
+
+taskCreateForm.addEventListener('submit', createNewTask)
+
+taskList.addEventListener('click', (event) => {
+  const task = document.querySelector('li')
+  
+  task.remove()
+})
