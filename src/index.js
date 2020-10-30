beb767189410ad689d7372047cad1c779bbd429d
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-10-29 18:55:24 -0600

    task priority dropdown with css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const taskCreateForm = document.getElementById('create-task-form')
 const newTaskInput = document.getElementById('new-task-description')
 const taskList = document.getElementById('tasks')
+const prioritySelector = document.getElementById('priority-level')
+const priorityLevels = ['Low', 'Medium', 'High']
 
+const priorityOptions = priorityLevels.forEach(level => {
+  const option = document.createElement('option')
+  option.textContent = level
+  option.value = level
+  option.id = level
+  prioritySelector.append(option)
+})
 
 const createNewTask = (event) => {
   event.preventDefault()
   
   const taskFormData = new FormData(event.target);
   const taskDescription = taskFormData.get('new-task-description')
+  const taskPriority = taskFormData.get('priority-level')
   
   const newTask = document.createElement('li')
   newTask.innerHTML = taskDescription
+  newTask.id = taskPriority
   
   const removeButton = document.createElement('button')
   removeButton.innerText = "X"
