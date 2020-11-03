2449181316ff1b116afdb0c5112f33dc3bfd78a3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-11-02 18:05:39 -0700

    sort button working with styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const taskCreateForm = document.getElementById('create-task-form')
 const newTaskInput = document.getElementById('new-task-description')
 const taskList = document.getElementById('tasks')
-const prioritySelector = document.getElementById('priority-level')
-const priorityLevels = ['Low', 'Medium', 'High']
 
-const priorityOptions = priorityLevels.forEach(level => {
+let newTasksList = []
+
+const prioritySelector = document.getElementById('priority-level')
+const priorityLevelsObject = {
+  low: {text: 'Low', value: 1},
+  medium: {text: 'Medium', value: 2},
+  high: {text: 'High', value: 3}
+}
+
+const priorityLevelsArray = Object.values(priorityLevelsObject)
+
+const priorityOptions = priorityLevelsArray.forEach(level => {
   const option = document.createElement('option')
-  option.textContent = level
-  option.value = level
-  option.id = level
+  option.textContent = level.text
+  option.value = level.value
+  option.id = level.text
+  
   prioritySelector.append(option)
 })
 
@@ -18,10 +28,12 @@ const createNewTask = (event) => {
   const taskFormData = new FormData(event.target);
   const taskDescription = taskFormData.get('new-task-description')
   const taskPriority = taskFormData.get('priority-level')
-  
+  console.log(taskPriority)
   const newTask = document.createElement('li')
   newTask.innerHTML = taskDescription
-  newTask.id = taskPriority
+  newTask.classList.add(priorityLevelsArray.find(priority => priority.value == taskPriority).text)
+  // newTask.id = priorityLevelsArray.find(priority => priority.value == taskPriority).text
+  newTasksList.push({task: taskDescription, priority: +taskPriority})
   
   const removeButton = document.createElement('button')
   removeButton.innerText = "X"
@@ -37,3 +49,19 @@ taskCreateForm.addEventListener('submit', createNewTask)
 const removeTask = (event) => {
   event.target.parentNode.remove()
 }
+
+const sortButton = document.querySelector('#sort-task')
+
+const sortTasks = (event) => {
+  taskList.innerHTML = ' '
+  let sortedArray = newTasksList.sort((a, b) => b.priority - a.priority)
+  sortedArray.forEach(taskObject => {
+    const sortedTaskItem = document.createElement('li')
+    let sortedTask = taskObject.task
+    sortedTaskItem.classList.add(priorityLevelsArray.find(priority => priority.value == taskObject.priority).text)
+    sortedTaskItem.innerHTML = sortedTask
+    taskList.appendChild(sortedTaskItem)
+  })
+}
+
+sortButton.addEventListener('click', sortTasks)
